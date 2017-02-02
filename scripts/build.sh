97373952f0953ac4570fb147794158fd09cd54fb
lambequeeugosto <lambequeeugosto@gmail.com>
2017-02-02 00:22:06 -0200

    Arquivos desnecessários foram deletados

--- scripts/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo -n "Doing PostCSS stuff..."
-postcss --use postcss-cssnext --local-plugins -d lib/ src/*.css
-echo "done"
-
-echo -n "Doing Babel stuff....."
-babel src/script.js -o lib/script.js --presets es2015 --no-babelrc --minified
-echo "done"
